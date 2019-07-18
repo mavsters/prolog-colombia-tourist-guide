3b2f9a19470b6ab7df52229a3c1cb408bb8befb4
Miguel Ángel <20430676+mavsters@users.noreply.github.com>
2019-07-18 14:33:44 -0500

    [Add] Base files

---
+++ Interfaz.pl
@@ -0,0 +1,106 @@
+%Interfaces gráficas
+%IMAGENES
+%
+:-use_module(library(pce)).
+:-pce_image_directory('./imagenes').
+:- use_module(library(pce_style_item)).
+:-consult('BaseConocimiento.pl').
+resource(img_principal,image,image('mapaColombia.jpg')).
+resource(icon,image,image('principal.jpg')).
+resource('Amazonas', image, image('amazonas.jpg')).
+resource('Antioquia', image, image('Antioquia.jpg')).
+resource('Arauca', image, image('arauca.jpg')).
+resource('Atlántico', image, image('atlantico.jpg')).
+resource('Bolívar', image, image('bolivar.jpg')).
+resource('Boyacá', image, image('boyaca.jpg')).
+resource('Caldas', image, image('caldas.jpg')).
+resource('Caquetá', image, image('caqueta.jpg')).
+resource('Casanare', image, image('casanare.jpg')).
+resource('Cauca', image, image('cauca.jpg')).
+resource('Cesar', image, image('cesar.jpg')).
+resource('Chocó', image, image('choco.jpg')).
+resource('Córdoba', image, image('cordoba.jpg')).
+resource('Cundinamarca', image, image('cundinamarca.jpg')).
+resource('Guainía', image, image('guainia.jpg')).
+resource('Guajira', image, image('guajira.jpg')).
+resource('Guaviare', image, image('guaviare.jpg')).
+resource('Huila', image, image('huila.jpg')).
+resource('Magdalena', image, image('magdalena.jpg')).
+resource('Meta', image, image('meta.jpg')).
+resource('Nariño', image, image('nariño.jpg')).
+resource('Nortedesantander', image, image('nortesantander.jpg')).
+resource('Putumayo', image, image('putumayo.jpg')).
+resource('Quindío', image, image('quindio.jpg')).
+resource('Risaralda', image, image('risaralda.jpg')).
+resource('Sanandres', image, image('sanandres.jpg')).
+resource('Sucre', image, image('sucre.jpg')).
+resource('Tolima', image, image('tolima.jpg')).
+resource('Valledelcauca', image, image('vallecauca.jpg')).
+resource('Vaupés', image, image('vaupes.jpg')).
+resource('Vichada', image, image('vichada.jpg')).
+resource('Santander', image, image('santander.jpg')).
+
+%%%
+%BUSQUEDA POR DEPTOS
+
+principal:-new(D,dialog('Guia turistica Colombiana')),
+new(BtnDep,button('Busqueda de dpto',message(@prolog,departamentos))),
+new(BtnMas,button('Mas Informacion',message(@prolog,mas))),
+new(LblImg,label(icon,resource(icon))),
+
+send_list(D,append,[BtnDep,BtnMas,LblImg]),
+send(D,open_centered).
+
+mas:-new(D,dialog('Acerca de Colombia')),
+new(LblTitulo,label(titulo, 'Colombia: Vive Colombia, Viaja por Ella' )),
+new(LblInfo,label(info,'Temporadas altas Diciembre-Febrero, Temporada mediaMarzo-Septiembre, Temporada Baja Octubre-Noviembre')),
+send(LblTitulo,font,font(tahoma,bold,20)),
+
+send_list(D,append,[LblTitulo,LblInfo]),
+send(D,open_centered).
+
+departamentos:-new(Dp,dialog('Atractivos por Departamento')),
+new(MenuDepto,menu('Departamentos:',cycle)),
+send_list(MenuDepto,append,['Amazonas','Antioquia','Arauca','Atlántico','Bolívar','Boyacá','Caldas','Caquetá','Casanare','Cauca','Cesar','Chocó','Córdoba','Cundinamarca','Guainía','Guaviare','Huila','La Guajira','Magdalena','Meta','Nariño','Norte de Santander','Putumayo','Quindío','Risaralda','San Andres y Providencia','Santander','Sucre','Tolima','Valle del Cauca','Vaupés','Vichada']),
+
+new(BtnHor,button('Buscar',message(@prolog,municipios,MenuDepto?selection))),
+new(LblImg,label(mapa,resource(img_principal))),
+
+send_list(Dp,append,[MenuDepto,BtnHor,LblImg]),
+send(Dp,open_centered).
+
+municipios(X):-new(D,dialog('municipios')),
+depa(X,L),
+new(MenuDepto,menu('Municipios',cycle)),
+new(BtnBus,button('Buscar',message(@prolog,info,MenuDepto?selection))),
+send_list(MenuDepto,append,L),
+
+new(LblImg,label(mapa,resource(X))),
+
+send_list(D,append,[MenuDepto,BtnBus,LblImg]),
+send(D,open_centered).
+
+info(Mun):-new(D,dialog('Informacion')),
+municipio(Mun,Ho,Atr),
+new(MenuHob,menu('Departamentos:',cycle)),
+send_list(MenuHob,append,Ho),
+new(MenuAtr,menu('Departamentos:',cycle)),
+send_list(MenuAtr,append,Atr),
+
+send_list(D,append,[MenuHob,MenuAtr]),
+send(D,open_centered).
+
+
+%BUSQUEDA POR HOBBIES
+hobbies:-new(Dp,dialog('Atractivos por Hobbies')),
+new(MenuHobbies,menu('Departamentos:',cycle)),
+send_list(MenuHobbies,append,['Avistamiento Ballenas','Bicicleta','Buggie','Buceo','Cabalgata','Caminata','Caminata Montaña','Camping','Canopy','Careteo','Coleo','Diversión Acuática','Escalar','Espeleología','Hamaca','Jet Ski','Jumping','Kayak','Observación Aves','Observación Aves, Fauna','Observación Aves, Flora y Fauna','Observación Fauna','Observación Flora','Observación Flora y Fauna','Observación Patrimonio Cultural','Observatorio Estrellas','Paracaidismo','Parapente','Playa','Rafting','Rappel','Relajación y Yoga','Sky','Snorkeling','Surf','Teleférico']),
+new(Op, menu('Atractivos', marked)),
+send(Op, append, norte), send(Op,append, sur),
+send(Op, size,size(300,300)),
+send(Dp, display, Op, point(100, 40)),
+new(BtnHor,button('Buscar',message(@prolog,infoPlantar))),
+
+send_list(Dp,append,[MenuHobbies,Op,BtnHor]),
+send(Dp,open_centered).
+
